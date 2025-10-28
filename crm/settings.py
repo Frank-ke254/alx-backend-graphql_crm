5f0ef5b24c1f59655020370c37042502f1f62ed2
Francis <kahotadfrajeh@gmail.com>
2025-10-28 11:41:25 +0300

    Add CRONJOBS for heartbeat and low stock updates

--- crm/settings.py
+++ crm/settings.py
@@ -69,6 +69,11 @@ TEMPLATES = [
     },
 ]
 
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
+]
+
 WSGI_APPLICATION = 'alx_backend_graphql_crm.wsgi.application'
 
 
